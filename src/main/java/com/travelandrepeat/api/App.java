6062aa3f24b2de1b9417bd86ddeeac801f681c92
travelandrepeatdev <travelandrepeatdev@gmail.com>
2026-01-06 18:45:21 -0600

    Fix import statement for DataSourceAutoConfiguration in App.java

--- src/main/java/com/travelandrepeat/api/App.java
+++ src/main/java/com/travelandrepeat/api/App.java
@@ -3,7 +3,7 @@ package com.travelandrepeat.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
 @Slf4j
