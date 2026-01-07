edbeb3f08aeb8005569b6ddf0e3ef6d25faeb02a
travelandrepeatdev <travelandrepeatdev@gmail.com>
2026-01-06 18:19:18 -0600

    Refactor DollarController to return ResponseEntity for exchange rate endpoint

--- src/main/java/com/travelandrepeat/api/controller/DollarController.java
+++ src/main/java/com/travelandrepeat/api/controller/DollarController.java
@@ -2,19 +2,23 @@ package com.travelandrepeat.api.controller;
 
 import com.travelandrepeat.api.service.BanxicoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("unused")
 @RestController
 @RequestMapping("/dollar")
 public class DollarController {
 
     @Autowired
-    BanxicoService banxicoService;
+    private BanxicoService banxicoService;
 
     @GetMapping(path = "/rate")
-    public String getDollarRate() {
-        return banxicoService.getExchangeRate();
+    public ResponseEntity<String> getDollarRate() {
+        String rate = banxicoService.getExchangeRate();
+        return ResponseEntity.status(HttpStatus.OK).body(rate);
     }
 }
